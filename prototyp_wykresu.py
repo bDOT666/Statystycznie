36a82cc446da62b94e057c065ce49c34887f2e5b
unknown <mateusz.zietara8721@gmail.com>
2020-01-29 23:34:17 +0100

    powinno dzialac z pewnymi ograniczeniami

---
+++ prototyp_wykresu.py
@@ -0,0 +1,63 @@
+import csv
+import statistics as stat
+import tkinter
+from tkinter import messagebox as msb
+from tkinter import scrolledtext
+from tkinter import ttk
+from tkinter.filedialog import *
+import scipy.stats as stats
+import numpy as np
+import math as mat
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+matplotlib.use('TkAgg')
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from tkinter import *
+
+with open('pokemon.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    lista = list(reader)
+    lista = np.array(lista)
+
+Nowa_lista = lista[1:,6:8]
+Nowa_lista = Nowa_lista.astype(np.float)
+
+
+def rysuj_wykres():
+
+    v = Nowa_lista[:, 0]
+    p = Nowa_lista[:, 1]
+
+    fig = Figure()
+    ax = fig.add_subplot(111)
+    ax.plot(v, color='#4285F4', linewidth=0.51)
+    ax.plot(p, color='#DB4437', linewidth=0.51)
+
+    ax.invert_yaxis()
+
+    ax.set_title("Wykres ", fontsize=16)
+    ax.set_ylabel("Y", fontsize=14)
+    ax.set_xlabel("X", fontsize=14)
+
+    rysuj = Toplevel()
+    canvas = FigureCanvasTkAgg(fig, master=rysuj)
+    canvas.get_tk_widget().pack()
+    canvas.draw()
+
+
+top= Tk()
+
+b1 = Button(top, text='mama', command=rysuj_wykres)
+b1.pack()
+
+top.mainloop()
+
+
+
+
+
+
+
