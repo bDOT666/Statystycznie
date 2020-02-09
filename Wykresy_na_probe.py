0d04d86d08b1edcaa6aeeb6d5b45dc949e9d1cf1
unknown <mateusz.zietara8721@gmail.com>
2020-02-09 23:15:24 +0100

    ta

--- Wykresy_na_probe.py
+++ Wykresy_na_probe.py
@@ -1,24 +1,21 @@
 import csv
 import statistics as stat
 import tkinter
+import numpy as np
+import pandas as pd
+import math as mat
 from tkinter import messagebox as msb
 from tkinter import scrolledtext
 from tkinter import ttk
 from tkinter.filedialog import *
 import scipy.stats as stats
-import pandas as pd
-import numpy as np
-import math as mat
 import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
 matplotlib.use('TkAgg')
-import numpy as np
-
-
+from tkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from tkinter import *
+
 
 
 
@@ -27,16 +24,57 @@ with open('C:\Officjum Inkwizytorskie\pliki\pokemon.csv', 'r') as f:
     lista = list(reader)
     lista = np.array(lista)
 
-Nowa_lista = lista[:,6:8]
-print(Nowa_lista[0])
+Nowa_lista = lista[:, 6:8]
+Naglowki = Nowa_lista[0]
+Nowa_lista = lista[1:, 6:8]
+
+print(Naglowki)
+
+def kolumny_do_wykresow():
+
+    slownik.clear()
+
+    for cb in lista_boxow:
+        cb.destroy()
+    lista_boxow.clear()
+
+    for kolumna in Naglowki:
+        slownik[kolumna] = tkinter.IntVar()
+        c = tkinter.Checkbutton(top, text=kolumna, variable=slownik[kolumna])
+        c.pack()
+        lista_boxow.append(c)
+
+
+def wyjmij_kolumny_wykresy():
+
+    do_wykresu = []
+    zmienne = []
+
+    for key, value in slownik.items():
+        if value.get() > 0:
+            i = Naglowki.tolist().index(key)
+            zmienne.append(i)
+
+    for i in zmienne:
+        do_wykresu.append(Nowa_lista[:, i])
+    return do_wykresu
 
 
 def rysuj_wykres():
+    do_wkresu = wyjmij_kolumny_wykresy()
+    do_wkresu = np.array(do_wkresu)
 
-    Nowa_lista1 = Nowa_lista[1:,:].astype(np.float)
+    do_wkresu = do_wkresu.astype(np.float)
+    # W tym miejscu masz wyjęte wybrane wcześniej kolumny
+    # 'do_wykresu' to array z tymi wybranymi kolumnami
+    # ich liczba nie musi sięzgadzać, ale jak zrobisz samo wywolywanie  wykresow to dodadm obostrzenie, żeby błąd
+    # wyskakiwał i podawał zakres ile możesz kolumn wybrać do danego wykresu
 
-    v = Nowa_lista1[:, 0]
-    p = Nowa_lista1[:, 1]
+
+    kolumny = do_wkresu.astype(np.float)
+
+    v = kolumny[0]
+    p = kolumny[1]
 
     fig = Figure()
     ax = fig.add_subplot(111)
@@ -57,16 +95,14 @@ def rysuj_wykres():
 
 top = Tk()
 
-b1 = Button(top, text='mama', command=rysuj_wykres)
+slownik = {}
+lista_boxow =[]
+
+b1 = Button(top, text='pokaż kolumny', command=kolumny_do_wykresow)
 b1.pack()
 
-e_nazwa1 = Entry(top)
-e_nazwa1.pack()
-
-e_nazwa2 = Entry(top)
-e_nazwa2.pack()
-
-
+b2 = Button(top, text='wyswietl zaznaczone kolumny i stworz wykres', command=rysuj_wykres)
+b2.pack()
 
 top.mainloop()
 
@@ -74,40 +110,6 @@ top.mainloop()
 
 
 
-def wybierz_kolumny():
-
-    filez = tkinter.filedialog.askdirectory(parent=window, title='Choose a file')
-
-    ent1.insert(20, filez)
-
-    dirs = os.listdir(filez)
-
-    # remove previous IntVars
-    intvar_dict.clear()
-
-    # remove previous Checkboxes
-    for cb in checkbutton_list:
-        cb.destroy()
-    checkbutton_list.clear()
-
-    for filename in dirs:
-        # create IntVar for filename and keep in dictionary
-        intvar_dict[filename] = tkinter.IntVar()
-
-        # create Checkbutton for filename and keep on list
-        c = tkinter.Checkbutton(window, text=filename, variable=intvar_dict[filename])
-        c.pack()
-        checkbutton_list.append(c)
-
-
-    for kolumna in Nowa_lista[0]:
-        # create IntVar for filename and keep in dictionary
-        Nowa_lista[kolumna] = tkinter.IntVar()
-
-        # create Checkbutton for filename and keep on list
-        c = tkinter.Checkbutton(window, text=kolumna, variable=Nowa_lista[kolumna])
-        c.pack()
-        checkbutton_list.append(c)
 
 
 
@@ -118,57 +120,3 @@ def wybierz_kolumny():
 
 
 
-
-
-def browse():
-
-    filez = tkinter.filedialog.askdirectory(parent=window, title='Choose a file')
-
-    ent1.insert(20, filez)
-
-    dirs = os.listdir(filez)
-
-    # remove previous IntVars
-    intvar_dict.clear()
-
-    # remove previous Checkboxes
-    for cb in checkbutton_list:
-        cb.destroy()
-    checkbutton_list.clear()
-
-    for filename in dirs:
-        # create IntVar for filename and keep in dictionary
-        intvar_dict[filename] = tkinter.IntVar()
-
-        # create Checkbutton for filename and keep on list
-        c = tkinter.Checkbutton(window, text=filename, variable=intvar_dict[filename])
-        c.pack()
-        checkbutton_list.append(c)
-
-def test():
-    for key, value in intvar_dict.items():
-        if value.get() > 0:
-            print('selected:', key)
-
-# --- main ---
-
-# to keep all IntVars for all filenames
-intvar_dict = {}
- # to keep all Checkbuttons for all filenames
-checkbutton_list = []
-
-window = tkinter.Tk()
-
-lbl = tkinter.Label(window, text="Path")
-lbl.pack()
-
-ent1 = tkinter.Entry(window)
-ent1.pack()
-
-btn1 = tkinter.Button(window, text="Select Path", command=browse)
-btn1.pack()
-
-btn1 = tkinter.Button(window, text="Test Checkboxes", command=test)
-btn1.pack()
-
-window.mainloop()
